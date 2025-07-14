be45c174c7a304b72800e900f10612711355c08d
Aneeshaik <aneesnafeesimmu@gmail.com>
2025-07-14 10:36:34 +0530

    env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
-    provideClientHydration(), 
-    provideAnimationsAsync(), 
-    provideFirebaseApp(() => initializeApp({"projectId":"todo-project-2109b","appId":"1:610553028347:web:612ae0eb0862f6614d9a5e","storageBucket":"todo-project-2109b.firebasestorage.app","apiKey":"AIzaSyDmvmS7NCZrrAxF2uAGYQEf8_uiCSuRArM","authDomain":"todo-project-2109b.firebaseapp.com","messagingSenderId":"610553028347","measurementId":"G-X1JYLMDVJN"})), provideFirestore(() => getFirestore())]
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
+  ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'todo-project-2109b',
+    appId: '1:610553028347:web:612ae0eb0862f6614d9a5e',
+    storageBucket: 'todo-project-2109b.firebasestorage.app',
+    apiKey: 'AIzaSyDmvmS7NCZrrAxF2uAGYQEf8_uiCSuRArM',
+    authDomain: 'todo-project-2109b.firebaseapp.com',
+    messagingSenderId: '610553028347',
+    measurementId: 'G-X1JYLMDVJN'
+  }
+};
